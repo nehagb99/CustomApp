4410f2800fdc00dfbfb0f77c334b502fccfd1efb
nehagb99 <neha.gb99@gmail.com>
2020-03-09 09:30:51 -0400

    Lab06b Added more elements to the main game page and began coding the functionalities of the questions and button

--- app/src/main/java/com/bagalkotneha/customapp/MainActivity.java
+++ app/src/main/java/com/bagalkotneha/customapp/MainActivity.java
@@ -21,29 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submitButton = findViewById(R.id.clickButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                System.out.println("Hello mom!");
-                // Log.i("firstButton", "Hi mom!");
-                Log.i("testButton","Hi dad!");
-                Log.i("testButton", responseText.getText().toString());
-                displayText.setText(displayText.getText()+" "+responseText.getText());
-            }
-        });
-        response2Text.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(!hasFocus) {
-                    if (response2Text.getText().toString().equals("TJ")) {
-                        displayText.setText("TJ Rocks!");
-                        response2Text.setText("");
-                        response2Text.setHint("That's a good name.");
-                    }
-                }
-            }
-        });
     }
 
     public void onClick(View view) {
--- app/src/main/java/com/bagalkotneha/customapp/b99activity.java
+++ app/src/main/java/com/bagalkotneha/customapp/b99activity.java
@@ -3,8 +3,11 @@ package com.bagalkotneha.customapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,11 +15,34 @@ import android.widget.Toast;
 public class b99activity extends AppCompatActivity {
 
     TextView question;
+    Chronometer chronometer;
+    int count = 0;
+    EditText responseText;
+    String[] questions;
+    String[] answers;
+    int score = 0;
+    TextView scoreText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.b99layout);
         question = findViewById(R.id.question);
+        responseText = findViewById(R.id.responseEditText);
+        scoreText = findViewById(R.id.score);
+
+        chronometer = (Chronometer) findViewById(R.id.simpleChronometer); // initiate a chronometer
+        chronometer.start(); // start a chronometer
+
+        Resources res = getResources();
+        questions = res.getStringArray(R.array.b99questions);
+        answers = res.getStringArray(R.array.b99answers);
+        question.setText(questions[count]);
+    }
+    public void onClick2(View view) {
+        String ans = responseText.getText().toString();
+        if (ans == answers[count]) {
+            score++;
+        }
     }
 }
